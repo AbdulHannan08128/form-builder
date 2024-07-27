19e50a25847fea2cb93e00a095db406ae23a21e9
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-07-27 09:50:37 +0530

    V1

--- app/dashboard/forms/inspect/submissions/[ID]/page.js
+++ app/dashboard/forms/inspect/submissions/[ID]/page.js
@@ -46,7 +46,8 @@ const Submissions = ({ params }) => {
 
     return (
         <div className="max-w-7xl mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Submissions</h1>
+            <h1 className="text-xl font-bold mb-4">Submissions</h1>
+            <h1 className="text-3xl font-bold mb-4">{formData.formName}</h1>
             {submissions.length === 0 ? (
                 <div className="text-center py-4">No submissions found.</div>
             ) : (
